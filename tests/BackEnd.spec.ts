9fcab8335cc7cc5b73d0aece30a15111e910f8de
ArthurFReis <arfure21@gmail.com>
2026-05-12 08:19:07 -0300

    backend 2

--- tests/BackEnd.spec.ts
+++ tests/BackEnd.spec.ts
@@ -1,64 +1,44 @@
 import { test, expect } from '@playwright/test';
 
 test.describe.parallel("API com paralelo", () => {
-     
-    const site = 'https://brasilapi.com.br/api'
 
+    test("tipos de teste de backend", async({request, page}) => {
+        let tiposTest = [1,2,3,4];
+        const site = 'https://brasilapi.com.br/api';
 
-    test("Bancos em geral", async({request, page}) => {
-        const response = await request.get(`${site}/banks/v1`);
-        expect(response.status()).toBe(200);
-       // console.log(response.text());
+        for(let i = 0; i < tiposTest.length; i++){
+
+            switch (tiposTest[i]) { 
+            case 1:
+                const response = await request.get(`${site}/banks/v1`);
+                expect(response.status()).toBe(200);
+                break;
+            case 2:
+                const response2 = await request.get(`${site}/banks/v1`);
+                //expect(response2.status()).toBe(400);
+                break;
+            case 3:
+                const response3 = await request.get(`${site}/banks/v1/1`);
+                console.log(response3.status());
+                expect(response3.status()).toBe(200);
+                break;  
+            case 4:
+                const response4 = await request.get(`${site}/cep/v1/89010025`);
+                expect(response4.status()).toBe(200);
+                const responsebody = JSON.parse(await response4.text());
+                console.log(responsebody);
+                break;
+
+            default:
+                console.log("Tipo de teste inválido!");
+                break;
+        }   
         
-    });
-
-    test("Bancos em geral a validação errada", async({request , page}) => {
-        const response = await request.get(`${site}/banks/v1`);
-        expect(response.status()).toBe(400);
-    });
-
-        test("banco com o código = 1 e imprimir o valor do status", async({request, page}) => {
-        const response = await request.get(`${site}/banks/v1/1`);
-        console.log(response.status());
-        //expect(response.status()).toBe(200);
-    });
-
-    test("Cep = 89010025 e mostrar o resultado do corpo do JSON", async({request, page}) => {
-        const response = await request.get(`${site}/cep/v1/89010025`);
-        expect(response.status()).toBe(200);
-        const responsebody = JSON.parse(await response.text());
-        console.log(responsebody);
+    }
 
     });
+
 });
 
-test.describe("API sem paralelo", () => {
-     
-    const site = 'https://brasilapi.com.br/api'
+    
 
-
-    test("Bancos em geral", async({request, page}) => {
-        const response = await request.get(`${site}/banks/v1`);
-        expect(response.status()).toBe(200);
-        
-    });
-
-    test("Bancos em geral a validação errada", async({request , page}) => {
-        const response = await request.get(`${site}/banks/v1`);
-        expect(response.status()).toBe(400);
-    });
-
-        test("banco com o código = 1 e imprimir o valor do status", async({request, page}) => {
-        const response = await request.get(`${site}/banks/v1/1`);
-        console.log(response.status());
-        //expect(response.status()).toBe(200);
-    });
-
-    test("Cep = 89010025 e mostrar o resultado do corpo do JSON", async({request, page}) => {
-        const response = await request.get(`${site}/cep/v1/89010025`);
-        expect(response.status()).toBe(200);
-        const responsebody = JSON.parse(await response.text());
-        console.log(responsebody);
-    });
-});
- 
